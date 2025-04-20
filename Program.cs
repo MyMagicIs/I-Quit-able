c4077fa950ba20086db6d86173c45facd06c314f
MyMagicIs <anushelke@gmail.com>
2025-04-20 14:56:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    // Define an interface called IQuittable
+    interface IQuittable
+    {
+        // Declare a method that classes must implement
+        void Quit();
+    }
+
+    // Define a basic Employee class that implements the IQuittable interface
+    class Employee : IQuittable
+    {
+        // Properties for Employee (you can expand as needed)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Implement the Quit() method from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine($"{FirstName} {LastName} has quit the job. 😢");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of Employee and set the properties
+            Employee emp = new Employee()
+            {
+                FirstName = "John",
+                LastName = "Rambo"
+            };
+
+            // Use polymorphism: create an object of type IQuittable
+            IQuittable quitter = emp;
+
+            // Call the Quit() method on the IQuittable interface
+            quitter.Quit();
+
+            // Pause to keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
